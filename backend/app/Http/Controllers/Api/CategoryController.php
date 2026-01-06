94beeb06eaaeb95c4ace5f371c09bef578d3c4e7
David Steven Gonzalez Padilla <davidsgonzalez98@hotmail.com>
2026-01-05 20:38:08 -0500

    Se ajusta el registro de categorias

--- backend/app/Http/Controllers/Api/CategoryController.php
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -32,7 +32,7 @@ class CategoryController extends Controller
     public function store(CategoryStoreRequest $request)
     {   
         try { 
-            $validated = $request->validated;
+            $validated = $request->validated();
             $category = $this->categoryService->createCategory($validated);
             return new CategoryResource($category);
         } catch (\Exception $e) {
